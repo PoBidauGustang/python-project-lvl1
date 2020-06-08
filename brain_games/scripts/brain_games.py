801a6f5c37aa429d23f47e850b935b0c72c703b2
Vlad Threee <threeeyesvl@gmail.com>
2020-06-09 01:35:37 +0300

    3-rd step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+from brain_games import cli
+
 def main():
-    print('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!\n')
+    cli.welcome_user()
 
 if __name__ == '__main__':
     main()
