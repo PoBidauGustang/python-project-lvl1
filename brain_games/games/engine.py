d9f9a46a7e61f1d3c0d86c0b081aea5a8b325ffa
Vlad Threee <threeeyesvl@gmail.com>
2020-06-29 23:59:04 +0300

    7.0

--- brain_games/games/engine.py
+++ brain_games/games/engine.py
@@ -16,3 +16,20 @@ def name_request():
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!\n'.format(name))
     return name
+
+
+def gcd(arg1, arg2):
+    """Give gcd of two integers.
+
+    Args:
+        arg1: The first argument.
+        arg2: The second argument.
+
+    Returns:
+        grand_cd.
+    """
+    while arg2 != 0:
+        var_help = arg2
+        arg2 = arg1 % arg2
+        arg1 = var_help
+    return arg1
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Even game."""
+from brain_games.games import code
+
+
+def main():
+    """Welcome to the Brain Games, play brain-even."""
+    code.game_gcd()
+
+
+if __name__ == '__main__':
+    main()
